ba6a901a6ac16060ab18e906d6d14d8c486a2c5a
soniharshilharshil <34932035+soniharshilharshil@users.noreply.github.com>
2018-01-18 21:10:25 -0800

    project will uploded

---
+++ book.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Harshil-PC
+ * Date: 1/1/2018
+ * Time: 8:28 PM
+ */
+?>
+<html>
+<title>The Toppers..!</title>
+<head>
+
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap  -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+
+    <!-- Magnific Popup -->
+
+<body>
+
+<div id="page">
+    <nav class="gtco-nav" role="navigation">
+        <div class="gtco-container">
+            <div class="row">
+                <div class="col-md-12 text-right gtco-contact">
+                    <ul class="">
+                        <li><a href="#"><i class="ti-mobile"></i> +91 8264686566 </a></li>
+                        <li><a href="http://twitter.com/gettemplatesco"><i class="ti-twitter-alt"></i> </a></li>
+                        <li><a href="https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession"><i class="icon-mail2"></i></a></li>
+                        <li><a href="https://www.facebook.com/"><i class="ti-facebook"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div id="gtco-logo"><a href="index.php">Toppers Family's World <em>.</em></a></div>
+                </div>
+                <div class="col-xs-8 text-right menu-1">
+                    <ul>
+                        <li class="active"><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li class="has-dropdown">
+                            <a href="services.php">Services</a>
+
+                            <ul class="dropdown">
+                                <li><a href="book.php">Hair</a></li>
+                                <li><a href="book.php">Nail</a></li>
+                                <li><a href="book.php">Spa</a></li>
+                                <li><a href="book.php">Color</a></li>
+                            </ul>
+                            <a href="education.php">Education</a>
+                        </li>
+                        <li class="has-dropdown">
+                            <a href="#">Dropdown</a>
+                            <ul class="dropdown">
+                                <li><a href="#">HTML5</a></li>
+                                <li><a href="#">CSS3</a></li>
+                                <li><a href="#">Sass</a></li>
+                                <li><a href="#">jQuery</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="portfolio.php">Portfolio</a></li>
+                        <li><a href="contact.php">Contact</a></li>
+                        <li><a href="login.php">Login / signup</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    </nav>
+
+
+
+
+
+
+
+
+<header id="gtco-header" class="gtco-cover" role="banner" style="background-image:url(images/img_bg_1.jpg);">
+    <div class="overlay"></div>
+    <div class="gtco-container">
+        <div class="row">
+        </div>
+    </div>
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,445 @@
+<!DOCTYPE HTML>
+
+<html>
+
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>The Toppers..!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Website Template by gettemplates.co" />
+	<meta name="keywords" content="free website templates, free html5, free template, free bootstrap, free website template, html5, css3, mobile first, responsive" />
+	<meta name="author" content="gettemplates.co" />
+
+  	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Themify Icons-->
+	<link rel="stylesheet" href="css/themify-icons.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<!-- Owl Carousel  -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+
+        <!-- Theme style  -->
+	<link rel="stylesheet" href="css/style.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+		
+	<div class="gtco-loader"></div>
+	
+	<div id="page">
+	<nav class="gtco-nav" role="navigation">
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12 text-right gtco-contact">
+					<ul class="">
+						<li><a href="#"><i class="ti-mobile"></i> +91 8264686566 </a></li>
+						<li><a href="http://twitter.com/gettemplatesco"><i class="ti-twitter-alt"></i> </a></li>
+						<li><a href="https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Ftab%3Dwm&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession"><i class="icon-mail2"></i></a></li>
+						<li><a href="https://www.facebook.com/"><i class="ti-facebook"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4 col-xs-12">
+					<div id="gtco-logo"><a href="index.php">Toppers Family's World <em>.</em></a></div>
+				</div>
+				<div class="col-xs-8 text-right menu-1">
+					<ul>
+						<li class="active"><a href="index.php">Home</a></li>
+						<li><a href="about.php">About</a></li>
+						<li class="has-dropdown">
+							<a href="services.php">Services</a>
+
+                            <ul class="dropdown">
+								<li><a href="book.php">Hair</a></li>
+								<li><a href="book.php">Nail</a></li>
+								<li><a href="book.php">Spa</a></li>
+								<li><a href="book.php">Color</a></li>
+							</ul>
+                            <a href="education.php">Education</a>
+						</li>
+						<li class="has-dropdown">
+							<a href="#">Dropdown</a>
+							<ul class="dropdown">
+								<li><a href="#">HTML5</a></li>
+								<li><a href="#">CSS3</a></li>
+								<li><a href="#">Sass</a></li>
+								<li><a href="#">jQuery</a></li>
+							</ul>
+						</li>
+						<li><a href="portfolio.php">Portfolio</a></li>
+						<li><a href="contact.php">Contact</a></li>
+                        <li><a href="login.php">Login / signup</a></li>
+					</ul>
+				</div>
+			</div>
+			
+		</div>
+	</nav>
+
+	<header id="gtco-header" class="gtco-cover" role="banner" style="background-image:url(images/img_bg_1.jpg);">
+		<div class="overlay"></div>
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-12 col-md-offset-0 text-left">
+					<div class="display-t">
+						<div class="display-tc">
+							<h1 class="animate-box" data-animate-effect="fadeInUp">Your Business To The Next Level</h1>
+							<h2 class="animate-box" data-animate-effect="fadeInUp">Welcome to the toppers World </em></h2>
+							<p class="animate-box" data-animate-effect="fadeInUp"><a href="book.php" class="btn btn-white btn-lg btn-outline">Book Appointment</a></p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<div id="gtco-features-3">
+		<div class="gtco-container">
+			<div class="gtco-flex">
+				<div class="feature feature-1 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-inner">
+						<span class="icon">
+							<i class="ti-search"></i>
+						</span>
+						<h3>Services</h3>
+						<p>We thankfull to annouce to our customers we are now in website and you can show all the services in our Website... </p>
+						<p><a href="services.php" class="btn btn-white btn-outline">Let's Go</a></p>
+					</div>
+				</div>
+				<div class="feature feature-2 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-inner">
+						<span class="icon">
+							<i class="ti-announcement"></i>
+						</span>
+						<h3>Announcdement</h3>
+						<p>We thankfull to annouced the new offers for the new Registration User..many type of offers can given by us.. </p>
+						<p><a href="#" class="btn btn-white btn-outline">Let's Go</a></p>
+					</div>
+				</div>
+				<div class="feature feature-3 animate-box" data-animate-effect="fadeInUp">
+					<div class="feature-inner">
+						<span class="icon">
+							<i class="ti-timer"></i>
+						</span>
+						<h3>Product</h3>
+						<p>Now we can sell our product in throught to website so happy to say our user can purchased our product in our site </p>
+						<p><a href="#" class="btn btn-white btn-outline">Let's Go</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+        <html>
+        <head>
+
+        </head>
+        <div class="aboutus-home">
+            <div class="aboutus-home-box">
+                <div class="aboutus-home-box1">
+                    <h1>
+                        How Toppers works
+                    </h1>
+                </div>
+                <div class="aboutus-home-box2">
+                    <img src="images/aboutus/service.png" alt="service" class="img-responsive"/>
+                    <h2>
+                        <span>Select Services</span><br/>
+                        Choose from a wide range of beauty services offered directly to your doorstep.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box3">
+                    <img src="images/aboutus/date.png" alt="date icon image" class="img-responsive"/>
+                    <h2>
+                        <span>Pick Date and Time</span><br/>
+                        Book beauty sessions at your convenience, anytime and anywhere in Bhuj.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box4">
+                    <img src="images/aboutus/receive.png" alt="recieve" class="img-responsive"/>
+                    <h2>
+                        <span>Receive Confirmation</span><br/>
+                        Upon booking, you'll immediately receive confirmation with relevant details.
+                    </h2>
+                </div>
+                <div class="aboutus-home-box5">
+                    <img src="images/aboutus/enjoy.png" alt="pamper yourself" class="img-responsive"/>
+                    <h2>
+                        <span>Enjoy the Experience</span><br/>
+                        Relax and enjoy the pampering because ladies, you deserve it.
+                    </h2>
+                </div>
+            </div>
+        </div>
+        </html>
+
+        <div id="gtco-portfolio" class="gtco-section">
+		<div class="gtco-container">
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+					<h2>Our Latest Works</h2>
+					<p>now you can see our latest work picture...</p>
+				</div>
+			</div>
+
+			<div class="row row-pb-md">
+				<div class="col-md-12">
+					<ul id="gtco-portfolio-list">
+						<li class="two-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_1.jpg); "> 
+							<a href="#" class="color-1">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>View the Earth from the Outer Space</h2>
+								</div>
+							</a>
+						</li>
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_2.jpg); ">
+							<a href="#" class="color-2">
+								<div class="case-studies-summary">
+									<span>Illustration</span>
+									<h2>Sleeping in the Cold Blue Water</h2>
+								</div>
+							</a>
+						</li>
+
+
+						<li class="one-half animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_3.jpg); ">
+							<a href="#" class="color-3">
+								<div class="case-studies-summary">
+									<span>Illustration</span>
+									<h2>Building Builded by Man</h2>
+								</div>
+							</a>
+						</li>
+						<li class="one-half animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_4.jpg); ">
+							<a href="#" class="color-4">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>The Peaceful Place On Earth</h2>
+								</div>
+							</a>
+						</li>
+
+						<li class="one-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_5.jpg); "> 
+							<a href="#" class="color-5">
+								<div class="case-studies-summary">
+									<span>Web Design</span>
+									<h2>I'm Getting Married</h2>
+								</div>
+							</a>
+						</li>
+						<li class="two-third animate-box" data-animate-effect="fadeIn" style="background-image: url(images/img_6.jpg); ">
+							<a href="#" class="color-6">
+								<div class="case-studies-summary">
+									<span>Illustration</span>
+									<h2>Beautiful Flowers In The Air</h2>
+								</div>
+							</a>
+						</li>
+					</ul>		
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-md-4 col-md-offset-4 text-center animate-box">
+					<a href="services.php" class="btn btn-white btn-outline btn-lg btn-block">See All Our Works</a>
+				</div>
+			</div>
+
+			
+		</div>
+	</div>
+
+	<div id="gtco-counter" class="gtco-section">
+		<div class="gtco-container">
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+					<h2>Fun Facts</h2>
+					<p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
+				</div>
+			</div>
+
+			<div class="row">
+				
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div class="feature-center">
+						<span class="icon">
+							<i class="ti-settings"></i>
+						</span>
+						<span class="counter js-counter" data-from="0" data-to="22070" data-speed="5000" data-refresh-interval="50">1</span>
+						<span class="counter-label">Creativity Fuel</span>
+
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div class="feature-center">
+						<span class="icon">
+							<i class="ti-face-smile"></i>
+						</span>
+						<span class="counter js-counter" data-from="0" data-to="97" data-speed="5000" data-refresh-interval="50">1</span>
+						<span class="counter-label">Happy Clients</span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div class="feature-center">
+						<span class="icon">
+							<i class="ti-briefcase"></i>
+						</span>
+						<span class="counter js-counter" data-from="0" data-to="402" data-speed="5000" data-refresh-interval="50">1</span>
+						<span class="counter-label">Projects Done</span>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 animate-box" data-animate-effect="fadeInLeft">
+					<div class="feature-center">
+						<span class="icon">
+							<i class="ti-time"></i>
+						</span>
+						<span class="counter js-counter" data-from="0" data-to="212023" data-speed="5000" data-refresh-interval="50">1</span>
+						<span class="counter-label">Hours Spent</span>
+
+					</div>
+				</div>
+					
+			</div>
+		</div>
+	</div>
+
+	<div id="gtco-products">
+		<div class="gtco-container">
+			<div class="row animate-box">
+				<div class="col-md-8 col-md-offset-2 text-center gtco-heading animate-box">
+					<h2>Products</h2>
+					<p>on this section you can show the product catelog</p>
+				</div>
+			</div>
+			<div class="row animate-box">
+				<div class="owl-carousel owl-carousel-carousel">
+					<div class="item">
+						<img src="images/img_1.jpg" alt="Free HTML5 Bootstrap Template by GetTemplates.co">
+					</div>
+					<div class="item">
+						<img src="images/img_2.jpg" alt="Free HTML5 Bootstrap Template by GetTemplates.co">
+					</div>
+					<div class="item">
+						<img src="images/img_3.jpg" alt="Free HTML5 Bootstrap Template by GetTemplates.co">
+					</div>
+					<div class="item">
+						<img src="images/img_4.jpg" alt="Free HTML5 Bootstrap Template by GetTemplates.co">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+
+	<footer id="gtco-footer" role="contentinfo">
+		<div class="gtco-container">
+			<div class="row row-p	b-md">
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3>About Us</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eos molestias quod sint ipsum possimus temporibus officia iste perspiciatis consectetur in fugiat repudiandae cum. Totam cupiditate nostrum ut neque ab?</p>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-md-push-1">
+					<div class="gtco-widget">
+						<h3>Links</h3>
+						<ul class="gtco-footer-links">
+							<li><a href="services.php">Services</a></li>
+							<li><a href="portfolio.php">portfolio</a></li>
+							<li><a href="contact.php">Contact us</a></li>
+							<li><a href="#">Terms of services</a></li>
+							<li><a href="about.php">About us</a></li>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-md-4">
+					<div class="gtco-widget">
+						<h3>Get In Touch</h3>
+						<ul class="gtco-quick-contact">
+							<li><a href="#"><i class="icon-phone"></i> +91 8264686566</a></li>
+							<li><a href="#"><i class="icon-mail2"></i> hsoni.123442@gmail.com</a></li>
+							<li><a href="#"><i class="icon-chat"></i> Live Chat</a></li>
+						</ul>
+					</div>
+				</div>
+
+			</div>
+
+			<div class="row copyright">
+				<div class="col-md-12">
+					<p class="pull-left">
+						<small class="block">&copy; 2017 Toppers . All Rights Reserved.</small>
+						<small class="block">Designed by HARSHIL VAIBHAV SUNIL</small>
+					</p>
+					<p class="pull-right">
+						<ul class="gtco-social-icons pull-right">
+							<li><a href="#"><i class="icon-twitter"></i></a></li>
+							<li><a href="#"><i class="icon-facebook"></i></a></li>
+							<li><a href="#"><i class="icon-linkedin"></i></a></li>
+							<li><a href="#"><i class="icon-dribbble"></i></a></li>
+						</ul>
+					</p>
+				</div>
+			</div>
+
+		</div>
+	</footer>
+	</div>
+
+	<div class="gototop js-top">
+		<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+	</div>
+	
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/magnific-popup-options.js"></script>
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
+
